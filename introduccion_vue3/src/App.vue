7677644c2cac8cfc40f53cc99fa553a6e698d4e3
Wilovy09 <SrWilovy09@gmail.com>
2023-11-20 08:58:34 -0600

    v-for

--- introduccion_vue3/src/App.vue
+++ introduccion_vue3/src/App.vue
@@ -1,12 +1,37 @@
 <script setup lang="ts">
-	const puntajeCrediticio = 700
+	const letras = ['a', 'b', 'c']
+	const users = {
+		name: 'Wilovy',
+		edad: 21,
+		confianza: true
+	}
 </script>
 
 <template>
-	<!-- v-show -->
-	<!-- aplica un display none si no se cumple la condicion -->
-	<p v-show="puntajeCrediticio >= 700">Test</p>
+	<!-- v-for -->
+	<!-- array | object | number | string | iterable -->
+	<ul>
+		<li v-for="(item, indice) in letras" :key="indice">
+			{{ item }}
+		</li>
+	</ul>
 	
+	<!-- number -->
+	<p>Listado de numeros</p>
+	<span v-for="numero in 10" :key="numero">
+		{{ numero }} /
+	</span>
+
+	<!-- string -->
+	<p>Deletrear: </p>
+	<ul>
+		<li v-for="letra in 'Wilovy'">{{ letra }}</li>
+	</ul>
+
+	<!-- objeto -->
+	<h1 v-for="(valor, propiedad, indice) in users" :key="indice">
+		{{ indice }} / {{ propiedad }} / {{ valor}}
+	</h1>
 
 </template>
 
