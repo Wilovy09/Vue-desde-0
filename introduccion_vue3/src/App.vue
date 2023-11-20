d8b28a9941b88e34b986995762dfc629e8b866f7
Wilovy09 <SrWilovy09@gmail.com>
2023-11-20 08:35:41 -0600

    v-if & v-else-if & v-else

--- introduccion_vue3/src/App.vue
+++ introduccion_vue3/src/App.vue
@@ -1,31 +1,15 @@
 <script setup lang="ts">
-	const saludo = "Hola a todos"
-	const btnHTML = '<button>Enviar</button>'
-	const color = 'color: red; background-color: black;'
-	const web = 'https://google.com'
-	const ahorros = 350;
+	const puntajeCrediticio = 800
 </script>
 
 <template>
-
-	<h1>Introduccion a Vue</h1>
-	<h2>{{ saludo }}</h2>
-	<p>{{ btnHTML }}</p>
-	<samp v-html="btnHTML" ></samp>
-	<p v-bind:style="color">TEST</p>
-	<a :href="web">Google</a>
-	{{ 8*8 }}
-	<h2 :style="`color:${ahorros >= 200 ? 'green ' : 'red'}`" >$ {{ ahorros }}</h2>
-	<h2 :class="ahorros >= 200 ? 'confondos' : 'sinfondo'" >$ {{ ahorros }}</h2> <!-- Ni idea porque no funciona-->
-	<h2>{{ saludo.toLocaleUpperCase() }}</h2>
+	<!-- v-if & v-else-if & v-else tienen que estar juntas una encima de otra -->
+	<p v-if="puntajeCrediticio >= 900">Excelente cliente</p>
+	<p v-else-if="puntajeCrediticio >= 600">Cliente regular</p>
+	<p v-else>Cliente pesimo</p>
 
 </template>
 
 <style scoped>
-	.sinfondo{
-		color: red;
-	}
-	.confondos{
-		color: green;
-	}
+	
 </style>
