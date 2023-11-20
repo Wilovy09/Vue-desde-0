e7dc38083828d81a3fa3533e52c8def4e5061337
Wilovy09 <SrWilovy09@gmail.com>
2023-11-20 08:40:41 -0600

    v-show

--- introduccion_vue3/src/App.vue
+++ introduccion_vue3/src/App.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-	const puntajeCrediticio = 800
+	const puntajeCrediticio = 700
 </script>
 
 <template>
-	<!-- v-if & v-else-if & v-else tienen que estar juntas una encima de otra -->
-	<p v-if="puntajeCrediticio >= 900">Excelente cliente</p>
-	<p v-else-if="puntajeCrediticio >= 600">Cliente regular</p>
-	<p v-else>Cliente pesimo</p>
+	<!-- v-show -->
+	<!-- aplica un display none si no se cumple la condicion -->
+	<p v-show="puntajeCrediticio >= 700">Test</p>
+	
 
 </template>
 
