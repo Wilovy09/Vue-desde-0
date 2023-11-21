7b41e48e2834d45d1dd80a2832876c0214b0b22b
Wilovy09 <SrWilovy09@gmail.com>
2023-11-21 00:05:04 -0600

    Computed

--- introduccion_vue3/src/App.vue
+++ introduccion_vue3/src/App.vue
@@ -1,41 +1,34 @@
 <!-- JS -->
-<!-- <script lang="ts">
-	import { defineComponent, ref } from 'vue';
-
-	export default defineComponent({
-		components:{},
-		setup(){
-			const ahorros = ref(500)
-
-			return {ahorros}
-		},
-		methods:{
-			depositar(){
-				this.ahorros += 100;
-			}
-		},
-		computed:{}
-	});
-</script> -->
-
 <!-- Incluyendo el SETUP en script -->
 <script setup lang="ts">
-	import { ref } from 'vue';
+	import { ref,computed } from 'vue';
 
 	const ahorros = ref(500);
 	const depositar = ()=>{
 		ahorros.value += 100;
 	}
 
+	const asignarClase = computed(()=>{
+		if (ahorros.value > 500){
+			return 'verde'
+		}
+		return 'rojo'
+	})
+
 </script>
 
 <!-- HTML -->
 <template>
-	<h1>Ahorros: ${{ ahorros }}</h1>
+	<h1 :class="asignarClase">Ahorros: ${{ ahorros }}</h1>
 	<button @click="depositar()">Depositar</button>
 </template>
 
 <!-- CSS -->
 <style scoped>
-	
+	.verde{
+		color: green;
+	}
+	.rojo{
+		color: rgb(255, 43, 43);
+	}
 </style>
