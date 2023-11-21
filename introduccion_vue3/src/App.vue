ffe6e2ecd39bc4c343e87e8b108538aa54a892ae
Wilovy09 <SrWilovy09@gmail.com>
2023-11-20 23:55:35 -0600

    Composition API

--- introduccion_vue3/src/App.vue
+++ introduccion_vue3/src/App.vue
@@ -1,26 +1,38 @@
 <!-- JS -->
+<!-- <script lang="ts">
+	import { defineComponent, ref } from 'vue';
+
+	export default defineComponent({
+		components:{},
+		setup(){
+			const ahorros = ref(500)
+
+			return {ahorros}
+		},
+		methods:{
+			depositar(){
+				this.ahorros += 100;
+			}
+		},
+		computed:{}
+	});
+</script> -->
+
+<!-- Incluyendo el SETUP en script -->
 <script setup lang="ts">
-	import { ref } from "vue";
+	import { ref } from 'vue';
 
-	const status = ref<boolean>(false)
-
-	const ahorros = ref<number>(500);
-
+	const ahorros = ref(500);
 	const depositar = ()=>{
-		status.value = !status.value;
-		
 		ahorros.value += 100;
-		console.log(ahorros);
 	}
+
 </script>
 
 <!-- HTML -->
 <template>
-	<!-- Reactividad -->
-	<h1>${{ ahorros }}</h1>
-	<button @click="depositar()">Depositar $100</button>
-	{{ status }}
-	<button @click="status = !status ">Cambiar status</button>
+	<h1>Ahorros: ${{ ahorros }}</h1>
+	<button @click="depositar()">Depositar</button>
 </template>
 
 <!-- CSS -->
